af116117731ec8fa18cccd7e9d110a9d7171350c
David Rodriguez <rgomar90@gmail.com>
2024-08-26 20:02:59 -0700

    add api dependency to WeatherViewModel

--- app/src/main/java/com/david/weathermvvm/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/david/weathermvvm/viewmodel/WeatherViewModel.kt
@@ -3,17 +3,20 @@ package com.david.weathermvvm.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.david.weathermvvm.model.repository.apiclient.ApiImplementation
-import com.david.weathermvvm.model.repository.apiclient.dto.CityResponse
+import com.david.weathermvvm.model.repository.apiclient.ApiRepository
+import com.david.weathermvvm.model.repository.apiclient.dto.Response
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class WeatherViewModel @Inject constructor( private var api: ApiImplementation) : ViewModel() {
+class WeatherViewModel @Inject constructor() : ViewModel() {
 
+    @Inject
+    lateinit var api: ApiRepository
 
-    private val _uiState = MutableLiveData<CityResponse>()
-    val uiState: LiveData<CityResponse> = _uiState
+    private val _uiState = MutableLiveData<Response>()
+    val uiState: LiveData<Response> = _uiState
+
 
     suspend fun getWeather(city: String) {
         _uiState.value = api.getWeather(city)
@@ -23,7 +26,7 @@ class WeatherViewModel @Inject constructor( private var api: ApiImplementation) 
         _uiState.value = api.getWeather(lat, lon)
     }
 
-    suspend fun getWeatherFavorite(city: String): CityResponse {
+    suspend fun getWeatherFavorite(city: String): Response {
         return api.getWeather(city)
     }
 
