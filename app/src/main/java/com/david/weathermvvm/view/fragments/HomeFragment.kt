933b87e0066c4fcecdc2274fc65cd98c7c3a7c37
David Rodriguez <rgomar90@gmail.com>
2024-08-26 20:01:12 -0700

    add new way to fulfill views

--- app/src/main/java/com/david/weathermvvm/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/david/weathermvvm/view/fragments/HomeFragment.kt
@@ -6,18 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.david.weathermvvm.R
 import com.david.weathermvvm.databinding.FragmentHomeBinding
+import com.david.weathermvvm.model.repository.apiclient.dto.Response
 import com.david.weathermvvm.viewmodel.WeatherViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
     private val weatherViewModel: WeatherViewModel by viewModels()
     private lateinit var binding: FragmentHomeBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,15 +26,24 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        lifecycleScope.launch {
+            weatherViewModel.getWeather("Mexicali")
+        }
+
         binding = FragmentHomeBinding.bind(view.rootView)
-        weatherViewModel.uiState.observe(viewLifecycleOwner) {
-            binding.tvCityNameHome.text = weatherViewModel.uiState.value?.location?.name
-            binding.tvTemparatureHome.text =
-                weatherViewModel.uiState.value?.current?.temp_c.toString()
-            binding.tvdateHome.text = weatherViewModel.uiState.value?.location?.localtime
+        weatherViewModel.uiState.observe(viewLifecycleOwner) { response ->
+            when(response ){
+                is Response.Success -> {
+                    val city = response.data
+                    binding.tvCityNameHome.text = city.location?.name
+                    binding.tvTemparatureHome.text = city.current?.temp_c.toString()
+                    binding.tvdateHome.text = city.location?.localtime
+                }
+
+                is Response.Failure -> TODO()
+            }
+
         }
         return view
     }
-
-
 }
